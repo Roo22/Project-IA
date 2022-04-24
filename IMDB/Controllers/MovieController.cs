3c174e3fce128fd78cf12d0b4460af9b26261035
Roo22 <rawan.mustafa75@gmail.com>
2022-04-24 03:27:00 +0200

    comimit

--- IMDB/Controllers/MovieController.cs
+++ IMDB/Controllers/MovieController.cs
@@ -16,5 +16,66 @@ namespace IMDB.Controllers
             var data = db.Movies.ToList();
             return View(data);
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (movie == null)
+            {
+                throw new ArgumentNullException(nameof(movie));
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(movie);
+            }
+            db.Movies.Add(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            var data = db.Movies.SingleOrDefault(c => c.MovieId == id);
+
+            if (data == null) return View("NotFound");
+            return View(data);
+        }
+        public ActionResult Edit(int id)
+        {
+            var data = db.Movies.SingleOrDefault(c => c.MovieId == id);
+
+            if (data == null) return View("NotFound");
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(movie);
+            }
+            var MoviesDb = db.Movies.SingleOrDefault(c => c.MovieId == movie.MovieId);
+            MoviesDb.MovieName = movie.MovieName;
+            MoviesDb.Description = movie.Description;
+            MoviesDb.ImageUrl = movie.ImageUrl;
+            MoviesDb.MovieCategory = movie.MovieCategory;
+            MoviesDb.Author = movie.Author;
+            MoviesDb.Director = movie.Director;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            var MoviesDb = db.Movies.SingleOrDefault(c => c.MovieId == Id);
+            db.Movies.Remove(MoviesDb);
+            db.SaveChanges();
+            return Json(new { result = 1 }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
